16519a11a77f1905538a2aab528db685267d2c52
李心愿 <108659908+xinyuanblue@users.noreply.github.com>
2024-11-09 12:36:14 +0800

    Create  data.js
    Signed-off-by: 李心愿 <108659908+xinyuanblue@users.noreply.github.com>

---
+++ js/data.js
@@ -0,0 +1,12 @@
+// 数据存储和处理
+const bookRoomData = {
+    totalCount: 100,  // 总数
+    yearlyGrowth: [
+        {year: 2018, count: 20},
+        {year: 2019, count: 35},
+        {year: 2020, count: 55},
+        {year: 2021, count: 75},
+        {year: 2022, count: 100}
+    ],
+    // 其他数据...
+};
